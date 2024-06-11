d363a8ff565aecb0deff6423586a5762acdb43e8
napatwongchr <napat.wongchr@gmail.com>
2024-06-11 09:47:33 +0700

    feat: complete mission 4

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -1,26 +1,38 @@
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import axios from "axios";
+
+// Endpoint: http://localhost:4000/products
+// Http method: GET
+// API ที่เอาไว้ดูข้อมูลสินค้าทั้งหมด
 
 function ProductHighlight() {
+  const [products, setProducts] = useState([]);
+
+  const getProductData = async () => {
+    const result = await axios.get("http://localhost:4000/products");
+    setProducts(result.data);
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
   return (
     <section className="bg-gray-200 py-8">
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          <ProductCard
-            imgSrc="https://picsum.photos/id/231/200/200"
-            productName="The Lorem Ipsum for photos."
-            productPrice={99.99}
-          />
-          <ProductCard
-            imgSrc="https://picsum.photos/id/232/200/200"
-            productName="Wireless Bluetooth Earbuds"
-            productPrice={79.99}
-          />
-          <ProductCard
-            imgSrc="https://picsum.photos/200/200"
-            productName="Stylish Cool Sunglasses in The World"
-            productPrice={79.99}
-          />
+          {products.map((item) => {
+            return (
+              <ProductCard
+                key={item.id}
+                imgSrc={item.image}
+                productName={item.name}
+                productPrice={item.price}
+              />
+            );
+          })}
         </div>
       </div>
     </section>
