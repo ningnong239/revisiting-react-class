3ad540554feb9945ac9b9ee5a4af095b54db6591
ningnong239 <ningnong2539@gmail.com>
2025-08-07 11:48:30 +0700

    feat: update ProductCard component to use props for image source, product name, and price

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 function ProductCard(props) {
+    const {imgSrc ,productName, price} = props;
   return (
     <div className="bg-white p-4 shadow-md">
       {/* 🛠️ Todo 1: ให้เปลี่ยน Value ของ `src` เป็นข้อมูลส่งเข้ามาใน Component ผ่าน Prop ที่ชื่อว่า `imgSrc`
@@ -15,16 +16,16 @@ function ProductCard(props) {
           />
           
       */}
-      <img
-        src="https://picsum.photos/id/231/200/200"
-        alt="The Lorem Ipsum for photos."
-        className="w-full mb-2"
-      />
+         <img
+            src={imgSrc}
+            alt="The Lorem Ipsum for photos."
+            className="w-full mb-2"
+          />
       <h3 className="text-lg font-semibold mb-1">
-        The Lorem Ipsum for photos.
+        {productName}
       </h3>
       <div className="flex justify-between items-center">
-        <p className="text-gray-600">$99.99</p>
+        <p className="text-gray-600">{price}</p>
         <button className="mt-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-4 rounded flex items-center">
           <ShoppingCartIcon className="h-4 w-4 mr-2" />
           Add to Cart
